98ee67105791bc91f7930cfe0d701e9290d0a861
torama <torben.amann@googlemail.com>
2014-04-10 14:07:28 +0200

    Test

--- src/main/java/dachkinder/HomePage.java
+++ src/main/java/dachkinder/HomePage.java
@@ -9,9 +9,6 @@ public class HomePage extends WebPage {
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
 		// TODO Add your page's components here
 
     }
--- src/test/java/dachkinder/TestHomePage.java
+++ src/test/java/dachkinder/TestHomePage.java
@@ -4,6 +4,9 @@ import org.apache.wicket.util.tester.WicketTester;
 import org.junit.Before;
 import org.junit.Test;
 
+import dachkinder.HomePage;
+import dachkinder.WicketApplication;
+
 /**
  * Simple test using the WicketTester
  */
